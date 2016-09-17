1c11c6984e2f731f379b7aac1867b6594d7dee30
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2016-09-18 01:12:50 +1000

    Everything except finish_date validation Hours of experiments and nothing...

--- simpletask/models/models.py
+++ simpletask/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from openerp.exceptions import ValidationError, UserError
 
 # class Status(models.Model):
 #     _name = 'simpletask.status'
@@ -19,7 +20,20 @@ class Task(models.Model):
     name = fields.Char('Title', size=64, required=True)
     image = fields.Binary(attachment=True)
     description = fields.Text(required=True)
-    finish_date = fields.Date(required=True)
+    finish_date = fields.Date(required=True, default=fields.Date.today) # TODO сделать валидацию поля
     status = fields.Selection(selection=STATUSES_SELECT, required=True)
     # status_id = fields.Many2one('simpletask.status', ondelete='set null',
     #                             string='Status', required=True)
+
+    @api.multi
+    @api.constrains('finish_date')
+    def _check_finish_date(self):
+        for fd in self:
+            if fd.finish_date.strftime('%Y-%m-%d') < fields.Date.today:
+                raise ValidationError(
+                    'Finish date must be greater or equal today')
+                # UserError(
+                #     'Finish date must be greater or equal today')
+        # if self.finish_date < fields.Date.today():
+        #     raise ValidationError(
+        #         'Finish date must be greater or equal today')
