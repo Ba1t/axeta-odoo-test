a155d51f41568d3d881b9eae2b6b2864a09df912
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2016-09-17 23:01:59 +1000

    Create "Task" model and views, menus for it

--- simpletask/models/models.py
+++ simpletask/models/models.py
@@ -2,14 +2,24 @@
 
 from openerp import models, fields, api
 
-# class simpletask(models.Model):
-#     _name = 'simpletask.simpletask'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+# class Status(models.Model):
+#     _name = 'simpletask.status'
 #
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+#     name = fields.Char('Title', required=True)
+
+
+STATUSES = ['Open', 'Needs offer', 'Offered', 'Approved', 'In progress',
+            'Ready', 'Verified', 'Closed']
+STATUSES_SELECT = [(status.lower(), status) for status in STATUSES]
+
+
+class Task(models.Model):
+    _name = 'simpletask.task'
+
+    name = fields.Char('Title', size=64, required=True)
+    image = fields.Binary(attachment=True)
+    description = fields.Text(required=True)
+    finish_date = fields.Date(required=True)
+    status = fields.Selection(selection=STATUSES_SELECT, required=True)
+    # status_id = fields.Many2one('simpletask.status', ondelete='set null',
+    #                             string='Status', required=True)
