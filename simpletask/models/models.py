94b1129c4442060f6f62be34f59dcf86bf08e1ce
Yuriy Voronin <voronin.yuriy.s@gmail.com>
2016-09-19 11:25:13 +1000

    Clean files

--- simpletask/models/models.py
+++ simpletask/models/models.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
-from openerp.exceptions import ValidationError, UserError
-
-# class Status(models.Model):
-#     _name = 'simpletask.status'
-#
-#     name = fields.Char('Title', required=True)
+from openerp.exceptions import ValidationError
 
 
-STATUSES = ['Open', 'Needs offer', 'Offered', 'Approved', 'In progress',
-            'Ready', 'Verified', 'Closed']
+STATUSES = ['Open', 'Needs offer', 'Offered', 'Approved',
+            'In progress', 'Ready', 'Verified', 'Closed']
 STATUSES_SELECT = [(status.lower(), status) for status in STATUSES]
 
 
@@ -22,8 +17,6 @@ class Task(models.Model):
     description = fields.Text(required=True)
     finish_date = fields.Date(required=True, default=fields.Date.context_today)
     status = fields.Selection(selection=STATUSES_SELECT, required=True)
-    # status_id = fields.Many2one('simpletask.status', ondelete='set null',
-    #                             string='Status', required=True)
 
     @api.one
     @api.constrains('finish_date')
